fb523232fd233063291407523d0cd7eb5f87f593
MrT3313 <rturgeon13@gmail.com>
2019-04-28 10:52:45 -0500

    <EnhancedTable />
    sorting sorrectly

--- revo_health/src/components/Homepage/TableTest_homepage.js
+++ revo_health/src/components/Homepage/TableTest_homepage.js
@@ -4,7 +4,7 @@ import React from "react";
 // REDUX
     import { connect } from "react-redux";
 
-    
+// MATERIAL UI
     import Table from "@material-ui/core/Table";
     import TableBody from "@material-ui/core/TableBody";
     import TableCell from "@material-ui/core/TableCell";
@@ -15,14 +15,14 @@ import React from "react";
     import Toolbar from "@material-ui/core/Toolbar";
     import Typography from "@material-ui/core/Typography";
     import Paper from "@material-ui/core/Paper";
-    import Checkbox from "@material-ui/core/Checkbox";
-    import IconButton from "@material-ui/core/IconButton";
     import Tooltip from "@material-ui/core/Tooltip";
     
     // STYLED ... will need
     import { withStyles } from "@material-ui/core/styles";
     
     // DONT NEED ... yet ... ever?
+    import Checkbox from "@material-ui/core/Checkbox";
+    import IconButton from "@material-ui/core/IconButton";
     import { lighten } from "@material-ui/core/styles/colorManipulator";
     import classNames from "classnames";
     import DeleteIcon from "@material-ui/icons/Delete";
@@ -49,6 +49,11 @@ notes:
 
 
 function desc(a, b, orderBy) {
+    console.log('SORTING FUNCTIONS -> desc')
+    console.log('SORTING FUNCTIONS -> desc: ', a)
+    console.log('SORTING FUNCTIONS -> desc: ', b)
+    console.log('SORTING FUNCTIONS -> desc: ', orderBy)
+
 	if (b[orderBy] < a[orderBy]) {
 		return -1;
 	}
@@ -71,6 +76,11 @@ function stableSort(array, cmp) {
 // This functions gets called in the table 
 // goes into 'cmp' argument in stableSort ABOVE 
 function getSorting(order, orderBy) {
+    console.log('SORTING FUNCTIONS -> getSorting')
+    console.log('SORTING FUNCTIONS -> getSorting: ', order)
+    console.log('SORTING FUNCTIONS -> getSorting: ', orderBy)
+
+
 	return order === "desc"
 		? (a, b) => desc(a, b, orderBy)
 		: (a, b) => -desc(a, b, orderBy);
@@ -119,30 +129,36 @@ function getSorting(order, orderBy) {
 // Create rows for TABLE HEAD ----- aka NOT the actual table but top bar
     const rows = [
         {   
-            id: "name",
+            id: "procedure",
             numeric: false,
             // disablePadding: true,
             label: "Procedure Name" },
-        {   id: "Doctor", 
+
+        {   id: "doctor", 
             numeric: false, 
             // disablePadding: true, 
             label: "Doctor" },
-        {   id: "Hospital", 
+
+        {   id: "hospital", 
             numeric: false, 
             // disablePadding: true, 
             label: "Hospital" },
-        {   id: "Cost", 
+
+        {   id: "procedureCost", 
             numeric: true, 
             // disablePadding: true, 
             label: "Cost" },
-        {   id: "Out_of_Pocket_Cost", 
+
+        {   id: "outOfPocket_PMT", 
             numeric: true, 
             // disablePadding: true, 
             label: "Out of Pocket Cost" },
-        {   id: "ins_pmt", 
+
+        {   id: "ins_PMT", 
             numeric: true, 
             // disablePadding: true, 
             label: "Insurance Payment" },
+            
         {   id: "ins_adjustment", 
             numeric: true, 
             // disablePadding: true, 
@@ -157,16 +173,24 @@ function getSorting(order, orderBy) {
 // -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- ** -- //
 
 class EnhancedTableHead extends React.Component {
+
 	createSortHandler = property => event => {
+        console.log('TABLE HEAD -> inside CreateSortHandler')
+        console.log('TABLE HEAD -> CreateSortHandler -> EVENT: ', event)
+        console.log('TABLE HEAD -> CreateSortHandler -> PROPERTY: ', property)
+
+
 		this.props.onRequestSort(event, property);
 	};
 
 	render() {
 		const {
-			// onSelectAllClick,
-			order,
-			orderBy,
-			// numSelected,
+			
+            // THESE PROPS ARE PASSED IN FROM THE TABLE
+                order,
+                orderBy,
+
+			
 			rowCount
 		} = this.props;
 
@@ -188,14 +212,12 @@ class EnhancedTableHead extends React.Component {
                                 {/* Tooltips display informative text when users hover over, focus on, or tap an element. */}
 								<Tooltip
                                     title="Sort"
-                                    
-                                    // placement={row.numeric ? "bottom-end" : "bottom-start"}
                                     placement='bottom'
-                                    
 									enterDelay={300}
 								>
                                     {/* THIS IS WHERE THE SORTING 
                                     STARTS WITH ONCLICK */}
+
 									<TableSortLabel
 										active={orderBy === row.id}
 										direction={order}
@@ -228,8 +250,8 @@ class EnhancedTableHead extends React.Component {
 class EnhancedTable extends React.Component {
 	state = {
 		order: "asc",
-		orderBy: "calories",
-		selected: [],
+		orderBy: "name",
+		// selected: [],
         
         // add data from props in Component Did Mount
         data: [],
@@ -258,7 +280,7 @@ class EnhancedTable extends React.Component {
                     this.props.dummyProcedures[i].insurance_adjustment
                 ) 
 
-                console.log(dataToPush)
+                // console.log(dataToPush)
                 fillData.push(dataToPush)
             }
         console.log('prepared data for tableState', fillData)
@@ -277,10 +299,20 @@ class EnhancedTable extends React.Component {
     // SORTING FUNCTION -- Table Head
     // THIS IS THE ONLY TABLE METHOD THAT INTERACTS WITH THE TABLE HEAD METHODS
         handleRequestSort = (event, property) => {
+            
+            console.log('TABLE METHOD -> handleRequestSort - EVENT: ',event)
+            console.log('TABLE METHOD -> handleRequestSort - PROPERTY: ',property)
+            
+            // set 'orderBy' to the name of the column you just selected (property)
             const orderBy = property;
+            
+            // logic to flip 'asc' & 'desc'
             let order = "desc";
-
-            if (this.state.orderBy === property && this.state.order === "desc") {
+            if (
+                this.state.orderBy === property 
+                && 
+                this.state.order === "desc"
+            ) {
                 order = "asc";
             }
 
@@ -324,11 +356,14 @@ class EnhancedTable extends React.Component {
                         // TABLE HEAD CREATED ABOVE
                         // -- ** -- ** -- ** -- ** -- ** -- ** -- // */}
                             <EnhancedTableHead
-                                numSelected={selected.length}
-                                order={order}
-                                orderBy={orderBy}
-                                // onSelectAllClick={this.handleSelectAllClick}
-                                onRequestSort={this.handleRequestSort}
+                                
+                                // SORTING PROPS SENT 
+                                // FROM table --> TO TABLE HEAD
+                                    order={order}
+                                    orderBy={orderBy}
+                                    onRequestSort={this.handleRequestSort}
+
+
                                 rowCount={data.length}
                             />
                         {/* // -- ** -- ** -- ** -- ** -- ** -- ** -- //
@@ -336,7 +371,8 @@ class EnhancedTable extends React.Component {
                         // -- ** -- ** -- ** -- ** -- ** -- ** -- // */}
 
 						<TableBody>
-							{stableSort(data, getSorting(order, orderBy))
+							{stableSort(
+                                data, getSorting(order, orderBy))
                                 
                                 // Sets up pagination
                                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -344,7 +380,7 @@ class EnhancedTable extends React.Component {
                                 // MAPs over what is returned by stableSort()
                                 .map(n => {
                                     
-                                    console.log(n)
+                                    // console.log(n)
                                     
 									return (
 										<TableRow
@@ -396,7 +432,7 @@ class EnhancedTable extends React.Component {
 						</TableBody>
 					</Table>
 				</div>
-                
+
 				<TablePagination
 					rowsPerPageOptions={[5, 10, 25]}
 					component="div"
