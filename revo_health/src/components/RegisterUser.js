b251ee5a6aeb4b30757d31b1c23f152ffdf8376a
MrT3313 <rturgeon13@gmail.com>
2019-04-29 22:00:45 -0500

    initial register function started

---
+++ revo_health/src/components/RegisterUser.js
@@ -0,0 +1,59 @@
+// REACT
+    import React, { Component } from "react";
+
+// Styled Components
+    import styled from "styled-components";
+
+// MATERIAL UI
+
+
+// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+// Assets
+// import logo from "../assets/RevLogo.svg";
+
+// STYLED COMPONENTS
+
+
+
+
+// -- *** START CODE *** -- //
+// -- *** START CODE *** -- //
+
+class RegisterUser extends Component {
+constructor(props) {
+        type: undefined
+        first_name: undefined
+        last_name: undefined
+        
+        username: undefined
+        password: undefined
+        email: undefined
+        
+        has_insurance: undefined
+        insurance_name: undefined
+    };
+    
+// When typing in the username/password, it gets saved into state.
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+    
+// Submitting the form will set a "User" in local storage. within 'withAuthenticate'
+// it checks to see if it's present, and if so, you're considered logged in.
+    handleLoginSubmit = e => {
+        e.preventDefault();
+        
+    };
+    
+    render() {
+        return (
+            <>
+                <div>HELLO FROM INSIDE REGISTER</div>
+            </>
+        );
+    }   
+
+}
+
+export default RegisterUser;
